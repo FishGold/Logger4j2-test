afe536bea4d9b74e1d047166d70b3abdb8ed5c78
FishGold <947229399@qq.com>
2015-12-03 21:32:45 +0800

    Initial commit

---
+++ src/com/zss/Servlet.java
@@ -0,0 +1,28 @@
+package com.zss;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2015/12/3.
+ */
+@WebServlet(name = "com.zss.Servlet",urlPatterns = "/test")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final Logger log = LogManager.getLogger();
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
+    }
+}
